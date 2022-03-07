2f5eabf1164d28a3f98efd199a52fc3766d1782b
Enei Pereira <eneipereira@outlook.com.br>
2022-03-07 14:14:10 -0300

    Realiza requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route/* , Link */ } from 'react-router-dom';
+import { Login, Search, Album, Favorites, Profile, ProfileEdit, NotFound } from './pages';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="loginName">
+            <input
+              name="loginName"
+              id="loginName"
+              data-testid="login-name-input"
+              placeholder="Insira um nome..."
+            />
+          </label>
+          <button
+            name="loginBtn"
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ }
+            onClick={ createUser({name: })}
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
